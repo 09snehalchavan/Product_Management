64f75cbff5ee4b6eaaac2609209a495378403896
Snehal <snehalkc09@gmail.com>
2026-09-02 09:05:39 +0530

    Changes

--- Product.c
+++ Product.c
@@ -12,6 +12,77 @@ struct Product
 struct Product p[100];
 int count = 0;
 
+void clear_input_buffer()
+{
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+        ;
+}
+
+int read_positive_int(const char *prompt)
+{
+    int value;
+
+    while (1)
+    {
+        printf("%s", prompt);
+        if (scanf("%d", &value) != 1)
+        {
+            printf("\nInvalid input. Please enter a number.\n");
+            clear_input_buffer();
+            continue;
+        }
+        clear_input_buffer();
+
+        if (value > 0)
+        {
+            return value;
+        }
+
+        printf("\nValue must be greater than 0.\n");
+    }
+}
+
+int read_non_negative_int(const char *prompt)
+{
+    int value;
+
+    while (1)
+    {
+        printf("%s", prompt);
+        if (scanf("%d", &value) != 1)
+        {
+            printf("\nInvalid input. Please enter a number.\n");
+            clear_input_buffer();
+            continue;
+        }
+        clear_input_buffer();
+
+        if (value >= 0)
+        {
+            return value;
+        }
+
+        printf("\nValue must be 0 or greater.\n");
+    }
+}
+
+void read_product_name(char name[100], const char *prompt)
+{
+    while (1)
+    {
+        printf("%s", prompt);
+        scanf("%99s", name);
+
+        if (name[0] != '\0')
+        {
+            return;
+        }
+
+        printf("\nProduct name cannot be empty.\n");
+    }
+}
+
 void create_product_file()
 {
     FILE *fp = fopen("Product.txt", "a+");
@@ -81,13 +152,24 @@ void save_product()
 void add_product()
 {
     int n;
-    printf("\nHow many products do you want to add? = ");
-    scanf("%d", &n);
 
-    if (n <= 0)
+    while (1)
     {
-        printf("\nPlease enter a valid number.\n");
-        return;
+        printf("\nHow many products do you want to add? = ");
+        if (scanf("%d", &n) != 1)
+        {
+            printf("\nInvalid input. Please enter a valid number.\n");
+            clear_input_buffer();
+            continue;
+        }
+        clear_input_buffer();
+
+        if (n <= 0)
+        {
+            printf("\nPlease enter a valid number.\n");
+            continue;
+        }
+        break;
     }
 
     if (count + n > 100)
@@ -106,23 +188,31 @@ void add_product()
         do
         {
             printf("Enter Product ID = ");
-            scanf("%d", &id);
+            if (scanf("%d", &id) != 1)
+            {
+                printf("\nInvalid product ID. Please enter a valid number.\n");
+                clear_input_buffer();
+                id = -1;
+                continue;
+            }
+            clear_input_buffer();
+
+            if (id <= 0)
+            {
+                printf("\nProduct ID must be greater than 0.\n");
+                continue;
+            }
 
             if (is_product_id_exist(id))
             {
                 printf("\nProduct ID already exists. Please enter a different ID.\n");
             }
-        } while (is_product_id_exist(id));
+        } while (id <= 0 || is_product_id_exist(id));
 
         p[index].product_id = id;
-        printf("Enter Product Name = ");
-        scanf("%s", p[index].product_name);
-
-        printf("Enter Product Price = ");
-        scanf("%d", &p[index].product_price);
-
-        printf("Enter Product Quantity = ");
-        scanf("%d", &p[index].product_quantity);
+        read_product_name(p[index].product_name, "Enter Product Name = ");
+        p[index].product_price = read_positive_int("Enter Product Price = ");
+        p[index].product_quantity = read_non_negative_int("Enter Product Quantity = ");
     }
 
     count += n;
@@ -160,7 +250,19 @@ void search_product()
     int found = 0;
 
     printf("\nEnter Product ID to search = ");
-    scanf("%d", &id);
+    if (scanf("%d", &id) != 1)
+    {
+        printf("\nInvalid product ID. Please enter a valid number.\n");
+        clear_input_buffer();
+        return;
+    }
+    clear_input_buffer();
+
+    if (id <= 0)
+    {
+        printf("\nProduct ID must be greater than 0.\n");
+        return;
+    }
 
     for (int i = 0; i < count; i++)
     {
@@ -187,8 +289,7 @@ void low_product()
     int threshold;
     int found = 0;
 
-    printf("\nEnter low stock threshold = ");
-    scanf("%d", &threshold);
+    threshold = read_non_negative_int("\nEnter low stock threshold = ");
 
     printf("\nProducts with quantity <= %d:\n", threshold);
 
@@ -217,7 +318,13 @@ void update_product()
     int found = 0;
 
     printf("\nEnter Product ID to update = ");
-    scanf("%d", &id);
+    if (scanf("%d", &id) != 1)
+    {
+        printf("\nInvalid product ID. Please enter a valid number.\n");
+        clear_input_buffer();
+        return;
+    }
+    clear_input_buffer();
 
     for (int i = 0; i < count; i++)
     {
@@ -228,14 +335,9 @@ void update_product()
             printf("Price: %d\n", p[i].product_price);
             printf("Quantity: %d\n", p[i].product_quantity);
 
-            printf("\nEnter new Product Name = ");
-            scanf("%s", p[i].product_name);
-
-            printf("Enter new Product Price = ");
-            scanf("%d", &p[i].product_price);
-
-            printf("Enter new Product Quantity = ");
-            scanf("%d", &p[i].product_quantity);
+            read_product_name(p[i].product_name, "\nEnter new Product Name = ");
+            p[i].product_price = read_positive_int("Enter new Product Price = ");
+            p[i].product_quantity = read_non_negative_int("Enter new Product Quantity = ");
 
             save_product();
             printf("\nProduct updated successfully.\n");
@@ -256,7 +358,19 @@ void delete_product()
     int found = 0;
 
     printf("\nEnter Product ID to delete = ");
-    scanf("%d", &id);
+    if (scanf("%d", &id) != 1)
+    {
+        printf("\nInvalid product ID. Please enter a valid number.\n");
+        clear_input_buffer();
+        return;
+    }
+    clear_input_buffer();
+
+    if (id <= 0)
+    {
+        printf("\nProduct ID must be greater than 0.\n");
+        return;
+    }
 
     for (int i = 0; i < count; i++)
     {
@@ -310,7 +424,16 @@ int main()
     do
     {
         printf("\nEnter your choice = ");
-        scanf("%d", &choice);
+        if (scanf("%d", &choice) != 1)
+        {
+            printf("\nInvalid choice. Please select a valid option.\n");
+            clear_input_buffer();
+            choice = 0;
+        }
+        else
+        {
+            clear_input_buffer();
+        }
 
         switch (choice)
         {
